26ff2ff85b52a666974614b7653346903205feda
herbert <herbert.pieris@japfa.com>
2022-12-02 15:07:38 +0700

    lambda code - add function get security group

--- lambda/cdkSecurityGroupFunction.py
+++ lambda/cdkSecurityGroupFunction.py
@@ -1,5 +1,13 @@
+import boto3;
+
+def getSecurityGroup():
+    ec2 = boto3.resource('ec2')
+    response = ec2.describe_security_groups()
+
+    return response
+
 def main(event, context):
-    print(event)
+    getSecurityGroup()
 
     return {
         'statusCode': 200,
