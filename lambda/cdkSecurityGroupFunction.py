43f915ee1f253bf36d3be5149ca3189610133b82
herbert <herbert.pieris@japfa.com>
2023-04-07 10:11:16 +0700

    print csvbody[x]

--- lambda/cdkSecurityGroupFunction.py
+++ lambda/cdkSecurityGroupFunction.py
@@ -373,17 +373,18 @@ def main(event, context):
             dichead=None
             dicbody=None
             for x in range(len(csvbody)-1):
-                if x==0:
-                    y= bytes.decode(csvbody[x])
-                    dichead=y.split(";")
-                if x!=0:
-                    y= bytes.decode(csvbody[x])
-                    dicbody=y.split(";")
-                    tmpdic = convertArrToDic(dichead,dicbody)
-                    if tmpdic["Type"].lower() == "inbound":
-                        response=authorizeSecurityGroupIngress(sggroupid,tmpdic)
-                    elif tmpdic["Type"].lower() == "outbound":
-                        response=authorizeSecurityGroupEgress(sggroupid,tmpdic)
+                print(csvbody[x])                
+                # if x==0:
+                #     y= bytes.decode(csvbody[x])
+                #     dichead=y.split(";")
+                # if x!=0:
+                #     y= bytes.decode(csvbody[x])
+                #     dicbody=y.split(";")
+                #     tmpdic = convertArrToDic(dichead,dicbody)
+                #     if tmpdic["Type"].lower() == "inbound":
+                #         response=authorizeSecurityGroupIngress(sggroupid,tmpdic)
+                #     elif tmpdic["Type"].lower() == "outbound":
+                #         response=authorizeSecurityGroupEgress(sggroupid,tmpdic)
                     
                 # response=None
                 # try:                
