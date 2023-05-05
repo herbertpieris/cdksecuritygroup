af0bfec4688d6109cde4ee78396d95c3ad5d9d04
herbert <herbert.pieris@japfa.com>
2023-05-05 14:30:18 +0700

    still do refactoring

--- lambda/cdkSecurityGroupFunction.py
+++ lambda/cdkSecurityGroupFunction.py
@@ -379,6 +379,18 @@ def compileIPPermissionIngress(tmpdic, IpPermissionIngress, mode):
                     ],
                     'ToPort': toPort,
                 })
+        else:
+            IpPermissionIngress.append({
+                'FromPort': fromPort,
+                'IpProtocol': validatePortocol(tmpdic),
+                'UserIdGroupPairs': [
+                    {
+                        'GroupId': tmpdic["UserIdGroupPairs"]["GroupId"],
+                        'Description': tmpdic["UserIdGroupPairs"]["Description"],
+                    },
+                ],
+                'ToPort': toPort,
+            })            
     return IpPermissionIngress
 
 ### compileIPPermissionEgress
