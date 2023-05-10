a69c86cda90e87893ad0d9860107f1efabd897aa
herbert <herbert.pieris@japfa.com>
2023-05-10 16:56:12 +0700

    refactoring code - 1 - test

--- lambda/cdkSecurityGroupFunction.py
+++ lambda/cdkSecurityGroupFunction.py
@@ -178,6 +178,7 @@ def writeAttachment(filename,value, mode):
 
         dicbody=None
         for x in range(len(value)):
+            print(value[x])
             dicbody=value[x].split(";")
             temp_my_file.writerow(dicbody)
         my_file.close()
